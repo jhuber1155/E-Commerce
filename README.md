a2813a4c707c01226559cff85862973aaafe82b2
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-10-19 22:35:15 -0700

    Added googledrive link for walkthrough video

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # E-Commerce
+
+Google Drive Link: https://drive.google.com/file/d/1fNTh-JGd82gAVmCv3k0CZqXLQF8MxMXp/view
--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const { NUMERIC } = require('sequelize');
 
 class Product extends Model {}
 
@@ -13,13 +14,23 @@ Product.init(
     autoIncrement: true
    },
    product_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false
    },
    price: {
-    type: DataTypes.DECIMAL (10, 2)
+    type: DataTypes.DECIMAL (10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
    },
    stock: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
    },
    category_id: {
     type: DataTypes.INTEGER,
